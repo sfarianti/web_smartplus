da38c56be52e1f5647725070e2e3f5700faad28c
rifanursyaida <165039788+rifanursyaida@users.noreply.github.com>
2025-06-21 12:57:13 +0700

    Update EnsureLoggedIn.php

--- app/Http/Middleware/EnsureLoggedIn.php
+++ app/Http/Middleware/EnsureLoggedIn.php
@@ -1,1 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureLoggedIn
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!session()->has('id_tentor') && !session()->has('id_user')) {
+            return redirect()->route('login')->withErrors([
+                'error' => 'Silakan login terlebih dahulu.',
+            ]);
+        }
+
+        return $next($request);
+    }
+}
 
